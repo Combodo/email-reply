1617507a44e77a65cc7734e987a2e25c6d8716d4
Stephen Abello <stephen.abello@combodo.com>
2021-09-02 17:17:12 +0200

    Update module version to 1.3.1-dev

--- module.email-reply.php
+++ module.email-reply.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'email-reply/1.3.0',
+	'email-reply/1.3.1-dev',
 	array(
 		// Identification
 		//
