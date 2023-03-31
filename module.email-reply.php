d2339e17f30f1099ba4a789a3868cb32242407b7
Molkobain <lajarige.guillaume@free.fr>
2023-03-31 15:20:07 +0200

    Increase module version to 1.4.3

--- module.email-reply.php
+++ module.email-reply.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'email-reply/1.4.2',
+	'email-reply/1.4.3',
 	array(
 		// Identification
 		//
